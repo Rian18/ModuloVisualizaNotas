83620c3c6ccbd4075359d24e999361c59e54161c
cassio <cassio@cassio-Aspire-E5-573>
2018-06-25 16:52:59 -0300

    Classes inciaisimplementaçao da classe controle

---
+++ src/java/testeDeSoftware/ControleServlet.java
@@ -0,0 +1,46 @@
+
+package testeDeSoftware;
+
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "controleServlet", urlPatterns = {"/inicial.html"})
+public class ControleServlet extends HttpServlet {
+
+    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Map<String, String> rotas = new HashMap<>();
+        rotas.put("/inicial.html", "testeDeSoftware.CommandInicial");
+    
+        String clazzName = rotas.get(request.getServletPath());
+          
+        try {
+            Comando comando = (Comando) Class.forName(clazzName).newInstance();
+            comando.exec(request, response);
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
+            response.sendError(500, "Erro: " + ex);
+            Logger.getLogger(ControleServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+    
+    @Override
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+}
